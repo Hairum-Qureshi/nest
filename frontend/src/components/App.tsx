b2dc7ac9ed3032fbd316bf91f6099a258dbb3a78
Hairum Qureshi <hqureshi@udel.edu>
2025-05-21 20:54:38 -0400

    updated imports and wrapped protected routes with ProtectedRoutesGuard component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import NotFound from "./pages/NotFound";
+import MainAuthentication from "./pages/authentication/MainAuthentication";
+import Dashboard from "./pages/dashboard/Dashboard";
+import ProtectedRoutesGuard from "./ProtectedRoutesGuard";
+import AIChat from "./pages/AIChat";
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Landing />} />
+				<Route path="/sign-up" element={<MainAuthentication />} />
+				<Route path="/sign-in" element={<MainAuthentication />} />
+				{/* <Route path="/dashboard" element={<Dashboard />} /> */}
+				<Route
+					path="/dashboard"
+					element={
+						<ProtectedRoutesGuard>
+							<Dashboard />
+						</ProtectedRoutesGuard>
+					}
+				/>
+				<Route
+					path="/chat"
+					element={
+						<ProtectedRoutesGuard>
+							<AIChat />
+						</ProtectedRoutesGuard>
+					}
+				/>
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
