e95ef31837806f9c6146483d304169265b6fc2c6
Hairum Qureshi <hqureshi@udel.edu>
2025-05-21 20:52:46 -0400

    created SideBar component

---
+++ frontend/src/components/SideBar.tsx
@@ -0,0 +1,13 @@
+export default function SideBar() {
+	return (
+		<div className="w-1/6 bg-[#09100d] p-4 min-h-screen shadow-md shadow-emerald-900">
+			<h1 className="text-sky-400 text-2xl font-semibold drop-shadow-[0_0_5px_#0b2630] mb-6">
+				Nest
+			</h1>
+			<div className="text-white space-y-2">
+				<h3>AI</h3>
+				<h3>Favorite Entries</h3>
+			</div>
+		</div>
+	);
+}
