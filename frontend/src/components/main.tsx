e688ed07d26bbf43ea681140eb46a02bf42f5b62
Hairum Qureshi <hqureshi@udel.edu>
2025-05-23 01:08:50 -0400

    set up React Query

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "../css/index.css";
-import App from "./App";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; // <-- if you want the devtools
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+			{/* <ReactQueryDevtools /> */}
+			{/* <- if you want the devtools component */}
+		</QueryClientProvider>
+	</React.StrictMode>
 );
