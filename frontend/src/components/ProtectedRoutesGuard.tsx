06494b614a96c64272e73c565f9393fd89730097
Hairum Qureshi <hqureshi@udel.edu>
2025-05-30 00:22:46 -0400

    added logic for rendering the sidebar for the Diary component page

--- frontend/src/components/ProtectedRoutesGuard.tsx
+++ frontend/src/components/ProtectedRoutesGuard.tsx
@@ -1,22 +1,25 @@
 import { Navigate, useLocation } from "react-router-dom";
 import SideBar from "./SideBar";
 
-// Dummy auth (replace with real auth logic)
-const isAuthenticated = true; // replace with real auth check
+//! Dummy auth (replace with real auth logic)
+const isAuthenticated = true; // TODO replace with real auth check
 
 export default function ProtectedRoutesGuard({
 	children
 }: React.PropsWithChildren) {
 	const location = useLocation();
 
-	const isDashboard = location.pathname.includes("/dashboard");
+	const isDashboard =
+		location.pathname.includes("/dashboard") ||
+		location.pathname.includes("/year");
 	const isAIChat = location.pathname.includes("/chat");
+	const isDiary = location.pathname.includes("/view");
 
 	if (!isAuthenticated) {
 		return <Navigate to="/sign-in" state={{ from: location }} replace />;
 	}
 
-	if (isDashboard || isAIChat) {
+	if (isDashboard || isAIChat || isDiary) {
 		return (
 			<div className="flex w-full h-screen">
 				<SideBar />
